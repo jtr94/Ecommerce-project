df947a6fe6e93c3ce2518cd69f7009a11572a92f
juantr94 <hart13ful@gmail.com>
2026-04-21 23:44:17 -0500

    Components: Orders, Chckout and Tracking added

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,9 +1,19 @@
 import { useState } from 'react'
 import {HomePage} from './pages/HomePage'
+import { Routes, Route } from 'react-router'
+import {CheckoutPage} from './pages/CheckoutPage'
+import { Orders } from './pages/Orders'
+import { Tracking } from './pages/Tracking'
 
 function App() {
   return (
-    <HomePage/>
+    <Routes>
+      <Route index element={<HomePage/>}/> 
+      <Route path='checkout' element={<CheckoutPage/>}/>
+      <Route path='orders' element={<Orders/>}/> 
+      <Route path='tracking' element={<Tracking/>}/>  
+    </Routes>
+    
   )
 }
 
