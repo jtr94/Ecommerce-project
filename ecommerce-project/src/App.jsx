5806ed253b489101eefc18cc88e0ffd53b5b1437
juantr94 <hart13ful@gmail.com>
2026-04-23 15:54:06 -0500

    Added Not Found Page

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router'
 import {CheckoutPage} from './pages/checkout/CheckoutPage'
 import { Orders } from './pages/Orders'
 import { Tracking } from './pages/Tracking'
+import { NotFound } from './pages/not-found/NotFound'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path='checkout' element={<CheckoutPage/>}/>
       <Route path='orders' element={<Orders/>}/> 
       <Route path='tracking' element={<Tracking/>}/>  
+      <Route path='*' element={<NotFound/>}/> 
     </Routes>
     
   )
---
+++ ecommerce-project/src/pages/not-found/NotFound.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Header} from './../../components/Header';
+import './Not-Found.css';
+
+export const NotFound = () => {
+  return (
+    <>
+    <title>Not Found</title>
+    <Header/>
+    <div className='not-found-page'>
+    <h1 >Not Found!</h1>
+    </div>
+    </>
+  )
+}
+
