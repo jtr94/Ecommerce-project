e4fbddfa889c5fe0447e0673cdec210193e66269
juantr94 <hart13ful@gmail.com>
2026-05-07 16:07:40 -0500

    Update tracking page implementation

--- ecommerce-project/src/pages/Tracking.jsx
+++ ecommerce-project/src/pages/Tracking.jsx
@@ -1,62 +1,86 @@
-import {React, useState, useEffect }from "react";
+import { React, useState, useEffect } from "react";
 import axios from "axios";
 import dayjs from "dayjs";
 import { useParams, Link } from "react-router";
 import './Tracking.css';
 import { Header } from "../components/Header";
 
-export const Tracking = ({cart}) => {  
+export const Tracking = ({ cart }) => {
   const params = useParams();
-  const {orderId, productId} = params;
+  const { orderId, productId } = params;
+
   const [order, setOrder] = useState(null);
-  console.log('orderId '+ orderId);
-  console.log('productId '+ productId);
-  
+
   useEffect(() => {
-    const fetchOrder = async()=> {
-      const response = await axios(`/api/orders/${orderId}?expand=products`)
-      setOrder(response.data)    
-    }
+    const fetchOrder = async () => {
+      const response = await axios(`/api/orders/${orderId}?expand=products`);
+      setOrder(response.data);
+    };
+
     fetchOrder();
   }, [orderId]);
-    
-  if (!orderId) { return null }
-  console.log(order)
+
+
+  if (!order) {
+    return <div>Loading...</div>;
+  }
+
+
+  const product = order.products.find(
+    (product) => product.productId === productId
+  );
+
+  const totalDeliveryTime = product.estimatedDeliveryTimeMs - order.orderTimeMs;
+  const timePassedMS = dayjs().valueOf() - order.orderTimeMs;
+  const deliveryPercent = (timePassedMS/totalDeliveryTime * 100 ) 
+  if (!product) {
+    return <div>Product not found</div>;
+  }
+
   return (
     <>
       <title>Tracking</title>
+
       <Header cart={cart} />
+
       <div className="tracking-page">
-        {
-          <div className="order-tracking">
-            <Link className="back-to-orders-link link-primary" to="/orders">
-              View all orders
-            </Link>
+        <div className="order-tracking">
 
-            <div className="delivery-date">Arriving on {dayjs(order.orderTimeMs).format('dddd, MMMM D')}</div>
+          <Link className="back-to-orders-link link-primary" to="/orders">
+            View all orders
+          </Link>
 
-            <div className="product-info">
-              Black and Gray Athletic Cotton Socks - 6 Pairs
-            </div>
-
-            <div className="product-info">Quantity: 1</div>
-
-            <img
-              className="product-image"
-              src="images/products/athletic-cotton-socks-6-pairs.jpg"
-            />
-
-            <div className="progress-labels-container">
-              <div className="progress-label">Preparing</div>
-              <div className="progress-label current-status">Shipped</div>
-              <div className="progress-label">Delivered</div>
-            </div>
-
-            <div className="progress-bar-container">
-              <div className="progress-bar"></div>
-            </div>
+          <div className="delivery-date">
+            Arriving on{" "}
+            {dayjs(product.estimatedDeliveryTimeMs).format(
+              "dddd, MMMM D"
+            )}
           </div>
-        }
+
+          <div className="product-info">
+            {product.product.name}
+          </div>
+
+          <div className="product-info">
+            Quantity: {product.quantity}
+          </div>
+
+          <img
+            className="product-image"
+            src={product.product.image}
+            alt={product.product.name}
+          />
+
+          <div className="progress-labels-container">
+            <div className="progress-label">Preparing</div>
+            <div className="progress-label current-status">Shipped</div>
+            <div className="progress-label">Delivered</div>
+          </div>
+          <div className="progress-bar-container">
+            <div className="progress-bar" style={{width:`${deliveryPercent > 100 ? 100 : deliveryPercent }%`}}></div>
+          </div>
+
+        </div>
       </div>
     </>
   );
